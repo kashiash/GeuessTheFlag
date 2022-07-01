64b73c85fa10f7aeed6aaff7ba439c9b6e1f130d
kashiash <kashiash@gmail.com>
2022-07-01 07:06:48 +0200

    Initial Commit

---
+++ GeuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GeuessTheFlag
+//
+//  Created by Jacek Placek on 01/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeuessTheFlag/GeuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeuessTheFlagApp.swift
+//  GeuessTheFlag
+//
+//  Created by Jacek Placek on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
